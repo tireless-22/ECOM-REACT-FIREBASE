2a2bae2724fc7fd72dcf197c19389136e0347757
karth <karthiknaveen22022002@gmail.com>
2022-01-11 11:40:15 +0530

    designed the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
 
 
-      <h1>hello this is from the app</h1>
+      {/* <h1>hello this is from the app</h1> */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Header.css'
+import { SiAmazon } from "react-icons/si";
+import { AiOutlineSearch } from "react-icons/ai"
+
+function Header() {
+	return (
+    <div className="header">
+     
+        <div className="header__log__div">
+          <SiAmazon className="header__logo" size="2rem" color="white" />
+        </div>
+        <div className="header__search">
+          <input className="header__searchInput" type="text" />
+          <div>
+            <AiOutlineSearch
+              className="search__icon"
+              size="2rem"
+              color="white"
+            />
+          </div>
+        </div>
+      
+      <div className="header__nav">
+        <div className="header_option">
+          <span className="header_option_lineOne">Hello Guest</span>
+          <span className="header_option_lineTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_option1_lineOne">Returns</span>
+          <span className="header_option_lineTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_option1_lineOne">Your</span>
+          <span className="header_option_lineTwo">Prime</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header

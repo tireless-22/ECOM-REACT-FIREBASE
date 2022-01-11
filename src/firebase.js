d762aa10d44fd31e9138565bc3fbb044590cf82b
karth <karthiknaveen22022002@gmail.com>
2022-01-11 08:28:33 +0530

    linked to firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1EUE_gglwE1Uj_mRX86pAED-EpuNpuHg",
+  authDomain: "ecomm-f4187.firebaseapp.com",
+  projectId: "ecomm-f4187",
+  storageBucket: "ecomm-f4187.appspot.com",
+  messagingSenderId: "332316453341",
+  appId: "1:332316453341:web:efeffe824aeca18fae6e34",
+  measurementId: "G-92EESR02LJ"
+};
